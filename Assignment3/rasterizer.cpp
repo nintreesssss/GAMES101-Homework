39ca280f2fb08ccd97311930a1bed15aadc865ff
nintreesssss <nintreesssss@gmail.com>
2026-03-07 22:11:24 +0800

    修复了高光

--- Assignment3/rasterizer.cpp
+++ Assignment3/rasterizer.cpp
@@ -273,10 +273,10 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t, const std::array<Eig
 
     for (int x = x0;x < x1;x++){
         for(int y = y0;y < y1;y++){
-            if(!insideTriangle(x,y,t.v)) continue;
             float px= x + 0.5f;
             float py= y + 0.5f;
-            auto [alpha, beta, gamma] = computeBarycentric2D(x, y, t.v);
+            if(!insideTriangle(px,py,t.v)) continue;
+            auto [alpha, beta, gamma] = computeBarycentric2D(px, py, t.v);
 
             float Z = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
             float zp = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
@@ -286,11 +286,10 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t, const std::array<Eig
             if(zp < depth_buf[index]){
                 depth_buf[index] = zp;
 
-                float weight = 1.0f / Z;
-                Vector3f interpolated_color = interpolate(alpha, beta, gamma, t.color[0], t.color[1], t.color[2], weight);
-                Vector3f interpolated_normal = interpolate(alpha, beta, gamma, t.normal[0], t.normal[1], t.normal[2], weight);
-                Vector2f interpolated_texcoords = interpolate(alpha, beta, gamma, t.tex_coords[0], t.tex_coords[1], t.tex_coords[2], weight);
-                Vector3f interpolated_shadingcoords = interpolate(alpha, beta, gamma, view_pos[0], view_pos[1], view_pos[2], weight);
+                auto interpolated_color = alpha * t.color[0] + beta * t.color[1] + gamma * t.color[2];
+                auto interpolated_normal = alpha * t.normal[0] + beta * t.normal[1] + gamma * t.normal[2];
+                auto interpolated_texcoords = alpha * t.tex_coords[0] + beta * t.tex_coords[1] + gamma * t.tex_coords[2];
+                auto interpolated_shadingcoords = alpha * view_pos[0] + beta * view_pos[1] + gamma * view_pos[2];
 
                 fragment_shader_payload payload(interpolated_color, interpolated_normal.normalized(), interpolated_texcoords, texture ? &*texture : nullptr);
                 payload.view_pos = interpolated_shadingcoords;
